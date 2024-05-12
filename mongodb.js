d327373b388737331a483c8021c012abb7abc203
inthra kumar <inthrak04@gmail.com>
2024-05-12 19:14:27 +0530

    iae update

--- mongodb.js
+++ mongodb.js
@@ -3,7 +3,7 @@ require("dotenv").config();
 
 
 
-mongoose.connect(`mongodb://127.0.0.1:27017/celestial_trust`, {
+mongoose.connect(`${process.env.MONGO_URI}`, {
   // useNewUrlParser: true,
   // useUnifiedTopology: true,
 });
